c914b5f7b2405edc9d8a09722f118f6c45809de4
mjbaum <150443188+mjbaum@users.noreply.github.com>
2023-11-16 19:34:16 +0000

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,48 @@
+from itertools import accumulate
+from networkx import stochastic_block_model, adjacency_matrix
+import random
+import numpy as np
+import pandas as pd
+
+def generate_groups(num_nodes, len_groups, randomize = False):
+    node_list = [i+1 for i in range(num_nodes)]
+    if randomize:
+        random.shuffle(node_list)
+    output = [node_list[x - y: x] for x, y in zip(
+        accumulate(len_groups), len_groups)]
+    return output
+
+def generate_group_labels(len_groups, randomize = False):
+    output = []
+    for i in range(len(len_groups)):
+        output = output + [i]*len_groups[i]
+    if randomize:
+        random.shuffle(output)
+    return output
+
+def generate_adjacency_matrix(len_groups, probabilities):
+    model = stochastic_block_model(sizes = len_groups, p=probabilities)
+    matrix = adjacency_matrix(model)
+    return matrix.toarray()
+
+def get_embedding(A, dimension = 4, type = 'left'):
+    decomp = np.linalg.svd(A, full_matrices = False)
+    U = decomp.U
+    D = decomp.S
+    D_half = np.diag(np.sqrt(D))
+    V = np.transpose(decomp.Vh)
+    if type == 'right':
+        right_embedding = V @ D_half
+        return right_embedding[:,0:dimension]
+    left_embedding = U @ D_half
+    return left_embedding[:,0:dimension]
+
+def group_by_label(matrix, labels):
+    labels = np.array(labels)
+    for label in set(labels):
+        print(label)
+        print(labels == label)
+        print(matrix[labels == label])
+
+
+
